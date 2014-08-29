770cc3448be0c4933abfc96d154243d0e5ed5101
Gabriel Georgescu <georgescu.gabriel90@gmail.com>
2014-08-30 01:21:45 +0300

    renamed:    AutoloadExample.php -> Math.php 	modified:   README.md

--- Math.php
+++ Math.php
@@ -5,7 +5,7 @@ namespace common\extension;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Math extends \yii\base\Widget
 {
     public function run()
     {
