486b11bf586ce5d2d99b743be40f4f483a1495a6
Gabriel Georgescu <georgescu.gabriel90@gmail.com>
2014-08-30 19:48:54 +0300

    modified:   math/Math.php

--- math/Math.php
+++ math/Math.php
@@ -12,7 +12,7 @@ class Math extends Widget
     /**
      * Format number
      *
-     * @param $number
+     * @param number $number
      * @param string $dDelimiter Decimal delimiter
      * @param null $precision number of decimals
      * @param string $tDelimiter Thousand delimiter
@@ -30,63 +30,66 @@ class Math extends Widget
     /**
      * Returns the sum of two numbers
      *
-     * @param $s1 number
-     * @param $s2 number
+     * @param number $s1
+     * @param number $s2
+     * @param null $precision
      * @return mixed
      */
-    public static function SumTwo($s1, $s2)
+    public static function SumTwo($s1, $s2, $precision = null)
     {
-        return static::formatNumber($s1 + $s2);
+        return static::formatNumber($s1 + $s2, $precision);
     }
 
     /**
      * Returns the sum of n numbers
      *
      * @param array $number
-     * @return int
+     * @param null $precision
+     * @return string
      */
-    public static function Sum($number = [])
+    public static function Sum($number = [], $precision = null)
     {
         $sum = 0;
         foreach ($number as $n) {
             $sum = static::SumTwo($sum, $n);
         }
-        return static::formatNumber($sum);
+        return static::formatNumber($sum, $precision);
     }
 
     /**
      * Returns decrease of two numbers
      *
-     * @param $s1
-     * @param $s2
+     * @param number $s1
+     * @param number $s2
+     * @param null $precision
      * @param bool $revert
      * @param bool $absolute
      * @return number
-     *
      */
-    public static function SubtractTwo($s1, $s2, $revert = false, $absolute = false)
+    public static function SubtractTwo($s1, $s2, $precision = null, $revert = false, $absolute = false)
     {
         if ($revert) {
             if ($absolute) {
-                return static::formatNumber(abs($s2 - $s1));
+                return static::formatNumber(abs($s2 - $s1), $precision);
             }
-            return static::formatNumber($s2 - $s1);
+            return static::formatNumber($s2 - $s1, $precision);
         }
         if ($absolute) {
-            return static::formatNumber(abs($s1 - $s2));
+            return static::formatNumber(abs($s1 - $s2), $precision);
         }
-        return static::formatNumber($s1 - $s2);
+        return static::formatNumber($s1 - $s2, $precision);
     }
 
     /**
      * Returns decrease of n numbers
      *
      * @param array $number
+     * @param null $precision
      * @param bool $revert
      * @param bool $absolute
      * @return string
      */
-    public static function Subtract($number = [], $revert = false, $absolute = false)
+    public static function Subtract($number = [], $precision = null, $revert = false, $absolute = false)
     {
         if ($revert) {
             $number = array_reverse($number);
@@ -96,84 +99,89 @@ class Math extends Widget
             $diff = static::SubtractTwo($diff, $n);
         }
         if ($absolute) {
-            return static::formatNumber(abs($diff));
+            return static::formatNumber(abs($diff), $precision);
         }
-        return static::formatNumber($diff);
+        return static::formatNumber($diff, $precision);
     }
 
     /**
      * Returns the multiplication of two numbers
      *
-     * @param $n1
-     * @param $n2
+     * @param number $n1
+     * @param number $n2
+     * @param null $precision
      * @param bool $absolute
      * @return string
      */
-    public static function MultipleTwo($n1, $n2, $absolute = false)
+    public static function MultipleTwo($n1, $n2, $precision = null, $absolute = false)
     {
         if ($absolute) {
-            return static::formatNumber(abs($n1 * $n2));
+            return static::formatNumber(abs($n1 * $n2),$precision);
         }
-        return static::formatNumber($n1 * $n2);
+        return static::formatNumber($n1 * $n2, $precision);
     }
 
     /**
      * Returns the multiplication of n numbers
      *
      * @param array $numbers
+     * @param null $precision
      * @param bool $absolute
      * @return string
      */
-    public static function Multiple($numbers = [], $absolute = false)
+    public static function Multiple($numbers = [], $precision = null, $absolute = false)
     {
         $multiple = 1;
         foreach ($numbers as $n) {
             $multiple = static::MultipleTwo($multiple, $n);
         }
         if ($absolute) {
-            return static::formatNumber(abs($multiple));
+            return static::formatNumber(abs($multiple), $precision);
         }
-        return static::formatNumber($multiple);
+        return static::formatNumber($multiple, $precision);
     }
 
     /**
      * Return value after subtract percent
      *
-     * @param $number
-     * @param $percent
+     * @param number $number
+     * @param number $percent
+     * @param null $precision
      * @return string
      */
-    public static function SubtractPercent($number, $percent)
+    public static function SubtractPercent($number, $percent, $precision = null)
     {
         $percent = $percent / 100;
         $percent = 1 - $percent;
 
-        return static::MultipleTwo($number, $percent);
+        return static::MultipleTwo($number, $percent, $precision);
     }
 
     /**
      * Return value after add percent
      *
-     * @param $number
-     * @param $percent
+     * @param number $number
+     * @param number $percent
+     * @param null $precision
      * @return string
      */
-    public static function AddPercent($number, $percent)
+    public static function AddPercent($number, $percent, $precision = null)
     {
         $percent = $percent / 100;
         $percent = 1 + $percent;
 
-        return static::MultipleTwo($number, $percent);
+        return static::MultipleTwo($number, $percent, $precision);
     }
 
     /**
      * Return value of factorial
      *
-     * @param $number
+     * @param number $number
+     * @param null $precision
      * @return string
      */
-    public static function Factorial($number)
+    public static function Factorial($number, $precision = null)
     {
-        return static::formatNumber(gmp_fact($number));
+        return static::formatNumber(gmp_fact($number), $precision);
     }
 }
